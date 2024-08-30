641f69596ef75741c6fbee2335bd5ae4cb021d17
NUSShao <gs20020225@gmail.com>
2024-08-30 15:53:58 +0800

    modified:   launch/rsp_sim.launch.py

--- launch/rsp_sim.launch.py
+++ launch/rsp_sim.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -24,6 +24,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py'
         )]),
+        # launch_arguments={'verbose': 'true'}.items(),
     )
 
     # Run the spawner node from the gazebo_ros package.
@@ -34,6 +35,7 @@ def generate_launch_description():
     
     # Launch everything!
     return LaunchDescription([
+        # SetEnvironmentVariable('GAZEBO_MODEL_PATH', '/home/nusshao/.gazebo/models'),
         rsp,
         gazebo,
         spawn_entity,
